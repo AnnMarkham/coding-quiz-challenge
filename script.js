e631dfb64ae67bf3173ed15f629835f87c60dd96
Ann Markham <ann.markham@outlook.com>
2020-10-14 11:34:51 -0700

    question array and psuedo code written

---
+++ script.js
@@ -0,0 +1,66 @@
+var startBtn = document.getElementById("start-quiz")
+var time = document.getElementById("time");
+var questionArrayEl = querySelector("wrapper");
+var askQuestion = document.getElementById("question")
+var answerEl;
+
+
+
+var questionArray = [
+  { question: "A very useful tool used during development and debugging for printing content to the debugger is:", choices: ["1.Javascript", "2.Terminal/Bash", "3.for loops", "4. console.log"], answer: "4" },
+  { question: "Arrays in JavaScript can be used to store ", choices: ["1.Numbers & Strings", "2. Other Arrays", "3. Booleans", "4. all of the above"], answer: "4" },
+  { question: " The condition in an if/else statement is enclosed with _______", choices: ["1. Quotation Marks", "2. Curyly Brackets", "P3. arentheses", "4. Square Brackets"], answer: "3" },
+  { question: "Commonly used data types do not include:", choices: ["Strings", "Booleans", "Alerts", "Numbers"], answer: "3" },
+]
+
+function start() {
+  var timeRemaining = 75;
+
+  var countDown = function () {
+    timeRemaining--;
+    time.innerhtml = timeRemaining;
+  }
+  if (timeRemaining === 0) {
+    //end the quiz and record the seconds remaining to input into Done and HighScore Slides
+  }
+  // get questions[0]  
+
+  // *** answer functions/events
+  //click on answer
+  //evalutate choice
+  //return - correct or wrong
+  //if wrong subtract time/
+
+  // check if there is time remaining (if current time is >0)
+  //check if there are questions remaining (i.e. if question [i] === 3 then no more questions)
+
+  // go to next question  (e.g. current question index number +1)  
+  // repeat function at *** above until no more questions or no more time -- if either -- go to game end function
+
+  //game end function 
+  // create an element <div> or <section>  
+  //with text showing "All done! "
+  // text "Your final score is" + timeRemaining "."
+  //input box <input type="text" name="initials" class"intials" placeholder="Enter Initials">
+  //submit button -- submit event -- ie storage of high score and initials
+
+  //create element <section High scores and each time submit button is clicked, appendChild? or append to the ordered list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  startButton.addEventListener("click", quizHandler);
